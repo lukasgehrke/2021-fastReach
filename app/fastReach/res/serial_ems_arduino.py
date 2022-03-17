d1b1ddf226a155ab5ec388fab09b113d39cb72f6
Lukas Gehrke <info@lukasgehrke.com>
2022-03-17 14:40:42 +0100

    clean up

--- app/fastReach/res/serial_ems_arduino.py
+++ app/fastReach/res/serial_ems_arduino.py
@@ -1,5 +1,5 @@
 import serial
-ems = serial.Serial(port='/dev/cu.usbmodem1101', baudrate=9600, timeout=.1)
+ems = serial.Serial(port='/dev/cu.usbmodem11201', baudrate=9600, timeout=.1)
 
 while True:
     ems_wait = input("press enter to send a EMS pulse: ") 
