bafff9ebd5f3584b6ea860bf40e2dbf16fff7a54
Lukas Gehrke <lukas.gehrke@tu-berlin.de>
2023-03-22 15:04:22 +0100

    Create pi_parse_events.m

---
+++ signal_processing/pi_parse_events.m
@@ -0,0 +1,17 @@
+function [eeglab_struct] = pi_parse_events(eeglab_struct)
+% CPS_PARSE_EVENTS parses a matlab struct by splitting key:value pairs
+% seprated by ';'. If no separator is found in the event, leave it as is
+    
+    for i = 1:numel(eeglab_struct.event)    
+        current_event = cellstr(strsplit(eeglab_struct.event(i).type, ';'));
+
+        for j=1:length(current_event)
+            key_val = cellstr(strsplit(current_event{j}, ':'));
+            eeglab_struct.event(i).(key_val{1}) = key_val{2};
+            if j==1
+                eeglab_struct.event(i).value = strcat(key_val{1}, ':', key_val{2});
+            end
+        end
+    end
+end
+
