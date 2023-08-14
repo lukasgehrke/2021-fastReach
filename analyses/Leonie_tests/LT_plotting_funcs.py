ed10b7a45b2826c0d4f8670fd697d4bf92ea21b6
leonie-ter <101427815+leonie-ter@users.noreply.github.com>
2023-08-14 08:51:06 +0200

    back into script

--- analyses/Leonie_tests/LT_plotting_funcs.py
+++ analyses/Leonie_tests/LT_plotting_funcs.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from statannotations.Annotator import Annotator
    
 # plot with two cats
-def plot_2_cats(data, x, y, hue, ylabel, xlabel,cats,palette):
+def plot_2_cats(data, x, y, hue, ylabel, xlabel,cats,palette,title):
     
     cats_0 = data[data[x]==cats[0]]
     cats_1 = data[data[x]==cats[1]]
@@ -73,8 +73,10 @@ def plot_2_cats(data, x, y, hue, ylabel, xlabel,cats,palette):
         fig.savefig('results/'+ title + '_' + ylabel + '_' + xlabel + '.png', format='png', transparent=False, bbox_inches='tight', dpi=300)
         fig.savefig('results/'+ title + '_' + ylabel + '_' + xlabel + '.eps', format='eps', transparent=True, bbox_inches='tight', dpi=300)
 
-# plot with three condition 
-def plot_3_cats(data, x, y, hue, ylabel, xlabel,cats,palette,data_means):
+# plot with three condition (including pID - mean lines)
+def plot_3_cats_pID(data, x, y, hue, ylabel, xlabel,cats,palette,data_means,title):
+
+
     
     cats_0 = data[data[x]==cats[0]]
     cats_1 = data[data[x]==cats[1]]
@@ -121,8 +123,53 @@ def plot_3_cats(data, x, y, hue, ylabel, xlabel,cats,palette,data_means):
         fig.savefig('results/'+ title + '_' + ylabel + '_' + xlabel + '.png', format='png', transparent=False, bbox_inches='tight', dpi=300)
         fig.savefig('results/'+title + '_' + ylabel + '_' + xlabel + '.eps', format='eps', transparent=True, bbox_inches='tight', dpi=300)
 
+def plot_3_cats(data, x, y, ylabel, xlabel,cats,palette,title):
+
+
+    
+    cats_0 = data[data[x]==cats[0]]
+    cats_1 = data[data[x]==cats[1]]
+    cats_2 = data[data[x]==cats[2]]
+
+    pairs = [(cats[0], cats[1],cats[1])]
+
+    
+
+    with sns.plotting_context('paper', font_scale = 1.8):
+
+        ### Create new plot
+        fig, ax = plt.subplots(1, 1, figsize=(3,5))
+        fig.patch.set_alpha(1)
+
+        sns.despine() #bottom=True, left=True
+         # show boxplots
+        ax = sns.boxplot(data = data,x = x, y = y,  palette= [palette[0]*len(pairs)])
+        for patch in ax.patches: # adapt alpha
+             r, g, b, a = patch.get_facecolor()
+             patch.set_facecolor((r, g, b, .6))
+
+                   
+        # show line connecting means
+        sns.pointplot(
+            data = data,x = x, y = y,
+            markers="s", size = 6, ci =None,color = palette[0],legend = True)
+
+
+        
+        # Label and show
+        # label_plot_for_subcats(ax)
+        ax.set_title(title)
+        ax.set_ylabel(ylabel)
+        ax.set_xlabel(xlabel)
+
+        plt.show()
+
+        fig.savefig('results/'+ title + '_' + ylabel + '_' + xlabel + '.png', format='png', transparent=False, bbox_inches='tight', dpi=300)
+        fig.savefig('results/'+title + '_' + ylabel + '_' + xlabel + '.eps', format='eps', transparent=True, bbox_inches='tight', dpi=300)
+
+
 # plot with three condition 
-def plot_3_cats_hue(data, x, y, hue, ylabel, xlabel,cats,palette,data_means,hue2):
+def plot_3_cats_hue(data, x, y, hue, ylabel, xlabel,cats,palette,data_means,hue2,title):
     
     cats_0 = data[data[x]==cats[0]]
     cats_1 = data[data[x]==cats[1]]
@@ -185,7 +232,7 @@ def plot_erp_timeline(data,id_vars, delete_vars, sample_rate,window_start,hue,pa
         data_long = pd.melt(data, id_vars= id_vars, var_name='timepoint', value_name='µV', col_level=None, ignore_index=True)
         
         # adjust time axis
-        data_long['timepoint'] = (((1/sample_rate) * (data_long["timepoint"].str.replace("erp","").astype(int)))*1000) +window_start
+        data_long['timepoint'] = (((1/sample_rate) * (data_long["timepoint"].str.replace("c_erp","").astype(int)))*1000) +window_start
         
 
         # plot mean (delete ci = None for shaded confidence interval)
@@ -222,7 +269,7 @@ def plot_erp_timeline_2(data_2, id_vars_2,sample_rate,window_start,hue,palette,t
         
         # adjust time axis
        
-        data_2_long['timepoint'] = (((1/sample_rate) * (data_2_long["timepoint"].str.replace("erp","").astype(int)))*1000) +window_start
+        data_2_long['timepoint'] = (((1/sample_rate) * (data_2_long["timepoint"].str.replace("c_erp","").astype(int)))*1000) + window_start
 
       
         ax = sns.lineplot(data = data_2_long, x = 'timepoint', y = 'µV',hue =hue,palette = palette[3:6],style = hue)
